5740d07fe804370d309c1fa8d8369ee5c86f8983
Anastasia <anovinnikova@yandex.ru>
2022-04-22 10:45:24 +0300

    Removed Sample scene, added Design Doc

--- CountingPrototype/Assets/Counter/Counter.cs
+++ CountingPrototype/Assets/Counter/Counter.cs
@@ -1,23 +1,20 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class Counter : MonoBehaviour
 {
     public Text CounterText;
 
-    private int Count = 0;
+    private int _count = 0;
 
     private void Start()
     {
-        Count = 0;
+        _count = 0;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Count += 1;
-        CounterText.text = "Count : " + Count;
+        _count += 1;
+        CounterText.text = "Count : " + _count;
     }
 }
