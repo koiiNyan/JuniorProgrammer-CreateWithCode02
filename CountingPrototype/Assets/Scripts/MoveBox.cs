388edc6abce5ade247ee2b9a8942757118dfe91b
Anastasia <anovinnikova@yandex.ru>
2022-04-22 15:51:34 +0300

    Added primitive cube, implemented mouse drag movement

---
+++ CountingPrototype/Assets/Scripts/MoveBox.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+
+public class MoveBox : MonoBehaviour
+{
+
+    private Vector3 GetMousePosition() =>
+        new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z + transform.position.z);
+
+    private void OnMouseDrag()
+    {
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(GetMousePosition());
+        transform.position = new Vector3(objPosition.x, transform.position.y, transform.position.z);
+    }
+
+}
