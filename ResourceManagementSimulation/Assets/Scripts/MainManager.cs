c123f4892c955d0f2acf82a3c58b2fe42f0929af
Anastasia <anovinnikova@yandex.ru>
2022-04-26 16:31:07 +0300

    Save color changes between sessions

--- ResourceManagementSimulation/Assets/Scripts/MainManager.cs
+++ ResourceManagementSimulation/Assets/Scripts/MainManager.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
@@ -7,6 +6,7 @@ public class MainManager : MonoBehaviour
     public static MainManager Instance;
 
     public Color TeamColor;
+
 
     private void Awake()
     {
@@ -18,5 +18,37 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        LoadColor();
     }
+
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
+    }
+
 }
