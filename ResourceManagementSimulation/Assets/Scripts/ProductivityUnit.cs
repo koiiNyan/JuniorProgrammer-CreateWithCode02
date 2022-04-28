5cd5fabf62249df0426789b668931c25cd250ffc
Anastasia <anovinnikova@yandex.ru>
2022-04-28 17:46:14 +0300

    Added Productivity Unit

---
+++ ResourceManagementSimulation/Assets/Scripts/ProductivityUnit.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductivityUnit : Unit
+{
+    private ResourcePile _сurrentPile = null;
+    public float ProductivityMultiplier = 2;
+
+    protected override void BuildingInRange()
+    {
+        if (_сurrentPile == null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
+
+            if (pile != null)
+            {
+                _сurrentPile = pile;
+                _сurrentPile.ProductionSpeed *= ProductivityMultiplier;
+            }
+        }
+    }
+
+    private void ResetProductivity()
+    {
+        if (_сurrentPile != null)
+        {
+            _сurrentPile.ProductionSpeed /= ProductivityMultiplier;
+            _сurrentPile = null;
+        }
+    }
+
+    public override void GoTo(Building target)
+    {
+        ResetProductivity(); 
+        base.GoTo(target); 
+    }
+
+    public override void GoTo(Vector3 position)
+    {
+        ResetProductivity();
+        base.GoTo(position);
+    }
+
+
+}
